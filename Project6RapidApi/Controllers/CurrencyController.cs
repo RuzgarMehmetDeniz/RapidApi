38182f81f0a2d5cc92c9992c5c9c915983e80dcd
Rüzgar Mehmet Deniz <ruzgarmehmetdeniz@gmail.com>
2026-04-17 15:44:27 +0300

    Add project files.

---
+++ Project6RapidApi/Controllers/CurrencyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project6RapidApi.Dtos;
+
+namespace Project6RapidApi.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "bee51651fdmshbbc375389ee61b2p180f6djsn683e69404831" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<CurrencyDto>(jsonData);
+                ViewBag.Currency = value.result;
+
+            }
+            return View();
+
+        }
+    }
+}
